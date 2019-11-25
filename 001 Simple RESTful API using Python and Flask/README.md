a4b7857b854b937615fb880f7e7a51c7e39dbe76
Lynard <lynardsalingujay@outlook.com>
2019-11-25 19:03:39 +1300

    updated

--- 001 Simple RESTful API using Python and Flask/README.md
+++ 001 Simple RESTful API using Python and Flask/README.md
@@ -4,3 +4,7 @@ python app.py
 * Running on http://127.0.0.1:5000/
 * Restarting with reloader
 ```
+## Invoke from curl
+```
+curl http://localhost:5000/todo/api/v1.0/tasks/2
+```
--- 001 Simple RESTful API using Python and Flask/app.py
+++ 001 Simple RESTful API using Python and Flask/app.py
@@ -17,9 +17,12 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'tasks': task[0]})
 
 @app.route('/')
 def index():
