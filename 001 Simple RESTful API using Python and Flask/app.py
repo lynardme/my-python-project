3dc9d3f25d03db51ff43f05f6f29e973bee5dcf2
Lynard <lynardsalingujay@outlook.com>
2019-11-26 18:00:36 +1300

    added post method

--- 001 Simple RESTful API using Python and Flask/app.py
+++ 001 Simple RESTful API using Python and Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -17,12 +17,29 @@ tasks = [
     }
 ]
 
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_tasks(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'tasks': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/')
 def index():
