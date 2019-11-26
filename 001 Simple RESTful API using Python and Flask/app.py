1ded24e0e1d613d5fe8505dd42a04646a8c26e33
Lynard <lynardsalingujay@outlook.com>
2019-11-26 18:25:50 +1300

    moved post image

--- 001 Simple RESTful API using Python and Flask/app.py
+++ 001 Simple RESTful API using Python and Flask/app.py
@@ -31,11 +31,15 @@ def create_task():
     return jsonify({'task': task}), 201
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
+def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'tasks': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 @app.errorhandler(404)
 def not_found(error):
